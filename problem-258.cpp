7407433a5a6f026e501a556ce70f797bd1c1cf62
chetan <chetankulkarni1605@gmail.com>
2026-09-02 19:40:12 +0530

    leetcode problem-258

---
+++ problem-258.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int addDigits(int num) {
+        while(num >= 10){
+            int sum = 0;
+            while(num > 0){
+                sum += num % 10;
+                num /= 10;
+            }
+            num = sum;
+        }
+        if(num == 0){
+            return 0;
+        }
+        return num;
+        
+    }
+};
