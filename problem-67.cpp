68cca3850d37634f38bf50bec2d544d73d7d8cf6
chetan <chetankulkarni1605@gmail.com>
2026-09-04 23:10:09 +0530

    leetcode problem-67

---
+++ problem-67.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        string ans = "";
+        int i = a.length() - 1;
+        int j = b.length() - 1;                                
+        int carry = 0;
+
+        ans.reserve(max(a.size(), b.size()) + 1);
+        while( i >= 0 || j >=0 || carry != 0 ){
+            int sum = carry;
+
+            if( i >= 0){
+                sum = sum + (a[i] - '0');
+                i--;
+            }
+            if( j >= 0){
+                sum = sum + (b[j] - '0');
+                j--;
+            }
+            
+            ans.push_back((sum % 2 )+ '0');
+            carry = sum / 2;
+        }
+        reverse(ans.begin() , ans.end());
+        return ans;
+    }
+};
